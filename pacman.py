cd64f9632a7af956ea5d0b66509251bbd68395a9
joeylmaalouf <joey.l.maalouf@gmail.com>
2015-01-28 02:28:59 -0500

    made ascii pacman

---
+++ pacman.py
@@ -0,0 +1,84 @@
+from subprocess import call
+import sys
+import time
+
+
+class Data(object):
+	def __init__(self, p, w, d, b):
+		self.collected = 0
+		self.total = 0
+		self.dead = False
+		self.px = 1 # If you change these, make sure to also change
+		self.py = 1 # his starting position on the board below!
+		self.player = p
+		self.wall = w
+		self.dot = d
+		self.blank = b
+		self.board = [
+		[w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+		[w, p, d, d, d, d, d, d, d, d, d, d, w, w, w, w, d, d, d, d, d, d, d, d, d, d, d, w],
+		[w, d, w, w, w, w, d, w, w, w, w, d, d, w, w, d, d, w, w, w, w, d, w, w, w, w, d, w],
+		[w, d, w, w, w, w, d, w, w, w, w, w, d, w, w, d, w, w, w, w, w, d, w, w, w, w, d, w],
+		[w, d, w, w, w, w, d, w, w, w, w, w, d, w, w, d, w, w, w, w, w, d, w, w, w, w, d, w],
+		[w, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, d, w],
+		[w, d, w, w, w, w, d, w, w, d, w, w, w, w, w, w, w, w, d, w, w, d, w, w, w, w, d, w],
+		[w, d, w, w, w, w, d, w, w, d, w, w, w, w, w, w, w, w, d, w, w, d, w, w, w, w, d, w],
+		[w, d, d, d, d, d, d, w, w, d, d, d, d, w, w, d, d, d, d, w, w, d, d, d, d, d, d, w],
+		[w, w, w, w, w, w, d, w, w, w, w, w, d, w, w, d, w, w, w, w, w, d, w, w, w, w, w, w],
+		[w, w, w, w, w, w, d, w, w, w, w, w, d, d, d, d, w, w, w, w, w, d, w, w, w, w, w, w],
+		[w, w, w, w, w, w, d, w, w, w, w, w, d, w, w, d, w, w, w, w, w, d, w, w, w, w, w, w],
+		[w, w, w, w, w, w, d, d, d, d, d, d, d, w, w, d, d, d, d, d, d, d, w, w, w, w, w, w],
+		[w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w]
+		]
+		for r in self.board:
+			for c in r:
+				if c == self.dot:
+					self.total += 1
+		# 127 dots
+		# try individuals with 150-200 inputs?
+
+
+def update(data, move):
+	data.board[data.py][data.px] = data.blank
+	if move is "l" and data.px > 0 and data.board[data.py][data.px-1] is not data.wall:
+		data.px -= 1
+	elif move is "r" and data.px < len(data.board[0])-1 and data.board[data.py][data.px+1] is not data.wall:
+		data.px += 1
+	elif move is "u" and data.py > 0 and data.board[data.py-1][data.px] is not data.wall:
+		data.py -= 1
+	elif move is "d" and data.py < len(data.board)-1 and data.board[data.py+1][data.px] is not data.wall:
+		data.py += 1
+	if data.board[data.py][data.px] == data.dot:
+		data.collected += 1
+	data.board[data.py][data.px] = data.player
+
+
+def display(data):
+	call("clear")
+	for r in data.board:
+		print("".join(r))
+
+
+def check(data):
+	if data.dead:
+		print("You lose!")
+		sys.exit()
+	if data.collected == data.total:
+		print("You win!")
+		sys.exit()
+
+
+def main(argv):
+	inputs = argv if argv[0] in ["l", "r", "u", "d"] else argv[1:]
+	data = Data("@", "#", ".", " ")
+	for i in inputs:
+		update(data, i)
+		# time.sleep(0.1)
+		# display(data)
+		check(data)
+	# print(data.collected)
+	return data.collected
+
+
+if __name__ == "__main__":
+	main(sys.argv)
